2665cf716b09c9bfad5a8b05e385868d08f5ae4d
mshokry <msn4eg@gmail.com>
2018-04-08 15:22:39 +0200

    v 0.6 Updated

--- rat_esp.ino
+++ rat_esp.ino
@@ -10,7 +10,7 @@
 #include <ESP8266httpUpdate.h>
 
 
-#define DEBUG 1
+#define DEBUG 0
 #if (DEBUG==1)
   #define PRINTDEBUGLN(STR) Serial.println(STR)
   #define PRINTDEBUG(STR) Serial.print(STR)
@@ -31,7 +31,7 @@
 #define API "?name="
 // WIFI Configuration
 char update_server[30] = "7elol.com";
-char update_server_page[30] = "/update/finger.php";
+char update_server_page[30] = "/update/config.php";
 char server[30] = "7elol.com";
 char server_page[30] = "/update/rfid.php";
 char server_port[6] = "80";
@@ -149,6 +149,7 @@ bool msg (String msg="alarm"){
   String url = String(server_page) + String(API) + String(PLACE) + String("&msg=");
   url += msg;
   url += String("&v=") + (ESP.getVcc());
+  url += String("&mac=") + String(WiFi.macAddress());
   PRINTDEBUGLN("Requesting URL:");
   PRINTDEBUGLN(url);
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -161,10 +162,115 @@ bool msg (String msg="alarm"){
 
   return true;
 }
-ADC_MODE(ADC_VDD);
+
+/*************************************************************************************
+ *  Requesting Config fromserver
+ *************************************************************************************/
+//Pingging main
+bool config (String msg="config"){
+   // Use WiFiClientSecure class to create TLS connection
+  WiFiClient client;
+  PRINTDEBUGLN("Connecting to:");
+  PRINTDEBUGLN(update_server);
+  if(!client.connect(update_server, 80))
+  {
+    PRINTDEBUGLN("Connection failed");
+    return false;
+  }
+  // URL request
+
+  String url = String(update_server_page) + String(API) + String(PLACE) + String("&msg=");
+  url += msg;
+  url += String("&v=") + (ESP.getVcc());
+  url += String("&mac=") + String(WiFi.macAddress());
+  PRINTDEBUGLN("Requesting URL:");
+  PRINTDEBUGLN(url);
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + update_server + "\r\n" +
+               "User-Agent: ratnest\r\n" +
+               "Connection: close\r\n\r\n");
+  PRINTDEBUGLN("Request sent");
+  PRINTDEBUGLN("Headers:");
+  PRINTDEBUGLN("========");
+  String line;
+  while(client.connected())
+  {
+    line = client.readStringUntil('\n');
+    PRINTDEBUGLN(line);
+    if(line == "\r")
+    {
+      PRINTDEBUGLN("========");      
+      PRINTDEBUGLN("Headers received");
+      //break;
+    }
+    if(line.startsWith("{")){
+      break;
+    }
+
+  }
+  //String line = client.readStringUntil('\n');
+  PRINTDEBUGLN("Reply was:");
+  PRINTDEBUGLN("==========");
+  PRINTDEBUGLN(line);
+  PRINTDEBUGLN("==========");
+  PRINTDEBUGLN("Closing connection");
+  // Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[600]);
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(line);
+  #if (DEBUG==1)  
+    json.printTo(Serial);
+  #endif
+  if (json.success()) {
+    PRINTDEBUGLN("\nparsed json");
+    strcpy(SSID, json["ssid"]);
+    strcpy(SSID_pwd, json["ssid_pwd"]);
+    PRINTDEBUGLN(SSID);
+    PRINTDEBUGLN(SSID_pwd);
+    if(json["update_server"]) 
+    strcpy(update_server, json["update_server"]);          
+    PRINTDEBUGLN(update_server);
+    if(json["update_server_page"]) 
+      strcpy(update_server_page, json["update_server_page"]);          
+    PRINTDEBUGLN(update_server_page);
+    //if(json["server"]) 
+    strcpy(server, json["server"]);
+    PRINTDEBUGLN(server);
+    if(json["server_page"])
+      strcpy(server_page, json["server_page"]);
+    PRINTDEBUGLN(server_page);
+    //if(json["server_port"]) 
+    //  strcpy(server_port, json["server_port"]);
+    //if(json["place"])
+    strcpy(PLACE, json["place"]);
+    PRINTDEBUGLN(PLACE);
+    if(json["ip"]) {
+      PRINTDEBUGLN("setting custom ip from config");
+      strcpy(static_ip, json["ip"]);
+      strcpy(static_gw, json["gateway"]);
+      strcpy(static_sn, json["subnet"]);
+      PRINTDEBUGLN(static_ip);
+/*            PRINTDEBUGLN("converting ip");
+      IPAddress ip = ipFromCharArray(static_ip);
+      PRINTDEBUGLN(ip);*/
+      return true;
+    } else {
+      PRINTDEBUGLN("no custom ip in config");
+      return false;
+    }
+  } else {
+    PRINTDEBUGLN("failed to load json config");
+    return false;
+  }
+      
+  return false;
+}
+
 /*************************************************************************************
  *  ////////////////////SETUP ////////////////
  *************************************************************************************/
+ADC_MODE(ADC_VDD);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(esp_ok,OUTPUT);
@@ -182,6 +288,7 @@ void setup() {
     Serial.setDebugOutput(true);
     Serial.println();
     Serial.println("Author:: Mahmoud Shokry");
+    Serial.println("version:: 0.6");
   #endif
   load_wifi();
   WiFi.mode(WIFI_STA);
@@ -235,10 +342,61 @@ void loop() {
     //delay( 1 );
     // Going To Sleep
   }else  if(digitalRead(esp_update)){
+    PRINTDEBUGLN("entering Update");
+    WiFi.forceSleepWake();
+    delay(1);
+    WiFi.mode(WIFI_STA);
+    //WiFi.setOutputPower(6);
+    IPAddress _ip,_gw,_sn;
+    _ip.fromString(static_ip);
+    _gw.fromString(static_gw);
+    _sn.fromString(static_sn);
+    delay( 1 );
+    WiFi.persistent( false );
+    WiFi.config( _ip, _gw, _gw, _sn );
+    //WiFi.begin( "Ebni.Eitesal", "eitesalngo" );
+    WiFi.begin( SSID, SSID_pwd );
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(100);
+      PRINTDEBUG(".");
+    }
+    PRINTDEBUGLN(WiFi.localIP());
+
+    if (config()) {
+      PRINTDEBUGLN("saving config");
+      digitalWrite(esp_ok, HIGH);
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& json = jsonBuffer.createObject();
+      json["server"] = server;
+      json["server_page"] = server_page;
+      json["update_server"] = update_server;
+      json["update_server_page"] = update_server_page;
+      json["place"] = PLACE;
+      json["ssid"] =  SSID;
+      json["ssid_pwd"] =  SSID_pwd;
+      json["ip"] = static_ip;
+      json["gateway"] = static_gw;
+      json["subnet"] = static_sn;
+      File configFile = SPIFFS.open("/config.json", "w");
+      if (!configFile) {
+        PRINTDEBUGLN("failed to open config file for writing");
+      }
+      #if (DEBUG==1)
+        json.prettyPrintTo(Serial);
+      #endif
+      json.printTo(configFile);
+      configFile.close();
+      //end save
+    }else{
+      digitalWrite(esp_ok, LOW);
+    }
+    PRINTDEBUGLN("All Done configuration");
+    WiFi.mode(WIFI_STA);
     digitalWrite(esp_ack,HIGH);
   }else if(digitalRead(esp_config)){
     PRINTDEBUGLN("entering configuration");
-
     WiFi.forceSleepWake();
     delay(1);
     WiFiManagerParameter custom_update("update_server", "Update Server", update_server, 34);
